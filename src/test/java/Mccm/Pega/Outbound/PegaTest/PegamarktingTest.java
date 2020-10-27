15a7989ab7ac24d8da607c73e4f60501db88adee
prout21 <prout21@dxc.com>
2020-10-27 16:52:45 +0530

    test

--- src/test/java/Mccm/Pega/Outbound/PegaTest/PegamarktingTest.java
+++ src/test/java/Mccm/Pega/Outbound/PegaTest/PegamarktingTest.java
@@ -69,6 +69,7 @@ public class PegamarktingTest extends TestBase {
 		 pegamarketpage.javaexecutor();
 		 pegamarketpage.CreateMulti();
 	//	 driver.switchTo().frame("PegaGadget1Ifr");
+		 driver.switchTo().defaultContent();
 		 pegamarketpage.Driversize();
 		 pegamarketpage.Create();
 		 pegamarketpage.MultiChannelCampaign(); 
