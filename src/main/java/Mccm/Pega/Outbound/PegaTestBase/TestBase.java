04ea6140dd01f7af99af8065d3f9e2e089aac278
prout21 <prout21@dxc.com>
2020-10-30 12:43:05 +0530

    push

--- src/main/java/Mccm/Pega/Outbound/PegaTestBase/TestBase.java
+++ src/main/java/Mccm/Pega/Outbound/PegaTestBase/TestBase.java
@@ -83,12 +83,12 @@ import Mccm.RESTful.APICall.DMPRTPNBAMobHash;
     	if(browserName.equals("chrome")) {
  
 
-         System.setProperty("webdriver.chrome.driver", chromedriverPath + "/" + chromeDriver);
+          System.setProperty("webdriver.chrome.driver", chromedriverPath + "/" + chromeDriver);
 
 
     		String projectPath = System.getProperty("user.dir");
 
-     //      System.setProperty("webdriver.chrome.driver", projectPath+"/chromedriver/chromedriver.exe");  
+   //       System.setProperty("webdriver.chrome.driver", projectPath+"/chromedriver/chromedriver.exe");  
 
     	
 
